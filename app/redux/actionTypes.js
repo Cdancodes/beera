ed473009873c5feca5c5cc8e4242e932a7332def
Cdancodes <chandancodesandmarketing@gmail.com>
2025-02-14 16:42:42 +0530

    added-redux-saga

---
+++ app/redux/actionTypes.js
@@ -0,0 +1,12 @@
+//Setings
+export const SET_IS_LOADING = 'SET_IS_LOADING';
+
+//Login
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+
+//Register
+export const REGISTER_REQUEST = 'REGISTER_REQUEST';
+
+//Contact
+export const CONTACT_REQUEST = 'CONTACT_REQUEST';
+
---
+++ app/redux/reducer/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import settings from "./settingReducer";
+
+
+const appReducer = combineReducers({
+    settings,
+})
+
+export default appReducer;
